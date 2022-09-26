468339175321ec3b41317426b7083abfa1088064
vsnupoudel <replytobishnu@gmail.com>
2022-09-26 16:45:32 +0530

    Update Fr-Bkt.py

--- Fr-Bkt.py
+++ Fr-Bkt.py
@@ -33,6 +33,17 @@ class Solution(object):
                 break
             
         return maxs
+    
+ # Hashmap
+def totalFruit( tree):
+    count, i = {}, 0
+    for j, v in enumerate(tree):
+        count[v] = count.get(v, 0) + 1
+        if len(count) > 2:
+            count[tree[i]] -= 1
+            if count[tree[i]] == 0: del count[tree[i]]
+            i += 1
+    return j - i + 1
                 
                 
         
